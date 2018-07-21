ff439744662717e412567f13376811e02c57ddec
Tori Mason <tori.mason@sbcglobal.net>
2018-07-21 11:59:27 -0500

    pulling from BIT and pushing to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,21 @@
 
-var apiKey = "";
+var apiKey = "973722cd55edcaaf54fc3f29432be2a4";
 var artist = "";
 var beginDate = "2018-09-09";
 var endDate = "2018-10-10";
 var queryURL = "";
 
+var config = {
+    apiKey: "AIzaSyDDNtVSjhQv8pEk7ZKDJNaPlBu0jeBZHpQ",
+    authDomain: "bandsintownapp.firebaseapp.com",
+    databaseURL: "https://bandsintownapp.firebaseio.com",
+    projectId: "bandsintownapp",
+    storageBucket: "",
+    messagingSenderId: "834818398888"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database()
 
 
 $(".find-em-btn").on("click" , function(){
@@ -29,5 +40,50 @@ $(".find-em-btn").on("click" , function(){
         console.log(response);
     
         console.log(queryURL);
+
+        var location = "";
+        var date = "";
+        var time = "";
+        var tickets = "";
+        var vanueName = "";
+        var latitude = "";
+        var longitude = "";
+        var lineup =[];
+
+
+
+        for(i = 0 ; i < response.length ; i ++){
+            location = response[i].venue.city + ", " + response[i].venue.region;
+            date = response[i].datetime.slice(0,10);
+            time = response[i].datetime.slice(11,16);
+            tickets = response[i].offers[0].status;
+            venueName = response[i].venue.name;
+            latitude = response[i].venue.latitude;
+            longitude = response [i].venue.longitude;
+            lineup = response[i].lineup;
+
+            console.log("Location " + [i + 1] + ": " + location );
+            console.log("Date " + [i + 1] + ": " + date );
+            console.log("Time " + [i + 1] + ": " + time );
+            console.log("Tickets " + [i + 1] + ": " + tickets );
+            console.log("Venue Name " + [i + 1] + ": " + venueName );
+            console.log("Latitude " + [i + 1] + ": " + latitude );
+            console.log("Longitude " + [i + 1] + ": " + longitude );
+            console.log("Lineup " + [i + 1] + ": " + lineup );
+
+            database.ref().push({
+                band: artist,
+                location: location,
+                date: date,
+                time: time,
+                tickets: tickets,
+            })
+        };
+
     });
+
 });
+
+
+
+
